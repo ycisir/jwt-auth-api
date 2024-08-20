0e83a74f2d5f8bdc0bfb347107fc615788c2c528
yasir <mohammadyasirkmt@gmail.com>
2024-08-20 11:35:48 +0530

    setting up host

--- django_auth_api1/settings.py
+++ django_auth_api1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jwt-auth-api-6b1y.onrender.com', '127.0.0.1']
 
 
 # Application definition
